cd29bd4f508291676327e1311724ff604fa2e10b
Vigor Andersson <vigoran@kth.se>
2026-05-17 23:25:28 +0200

    Fix #19: Implement control surfaces and organize prefabs.

---
+++ unity/AutonomousAerialSystems/Assets/Scripts/Aerodynamics/ControlSurface.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+/// <summary>
+/// This component is used on control surfaces in order to adjust deflection
+/// </summary>
+public class ControlSurface : MonoBehaviour
+{
+    [Header("Parameters")]
+    public ControlSurfaceType surfaceType;
+    public Vector3 RotationAxis;
+    public float MaxDeflection;
+
+    [Header("Input")]
+    public float Input;
+    private Quaternion _initialLocalRotation;
+
+    void Start()
+    {
+        _initialLocalRotation = transform.localRotation;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Rotates the control surface to the product of the input and max deflection
+    /// </summary>
+    /// <param name="input">Rotation input (between -1 and 1)</param>
+    public void DeflectSurface(float input)
+    {
+        float normalizedInput = Mathf.Clamp(input, -1f, 1f);
+        float angle = input * MaxDeflection;
+
+        transform.localRotation = _initialLocalRotation * Quaternion.AngleAxis(angle, RotationAxis);
+    }
+}
