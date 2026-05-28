6aecf017050f339b1fb20af08f57fc75e7e3a515
va-universe <vigor.andersson@icloud.com>
2026-05-28 16:05:41 +0200

    Work on #52, Fix #61: Implement lift coefficient calculation and zero lift angle.

--- unity/AutonomousAerialSystems/Assets/Scripts/Aerodynamics/WingSurface.cs
+++ unity/AutonomousAerialSystems/Assets/Scripts/Aerodynamics/WingSurface.cs
@@ -8,9 +8,12 @@ public class WingSurface : MonoBehaviour
     private Rigidbody _rb;
     private SecondAircraftController _controller;
 
-    [Header("Parameters")]
+    [Header("Airfoil")]
     public WingAxis LiftAxis;
-    public float ControlSurfaceRatio;
+    public float InitialZeroLiftAngle;
+
+    [Header("Control Surface")]
+    public float DeflectionCoefficient;
     public float ControlSurfaceDeflection;
     private float _totalSurfaceArea;
 
@@ -34,7 +37,70 @@ public class WingSurface : MonoBehaviour
 
     private void ApplyForces()
     {
+        Vector3 velocity = _rb.GetPointVelocity(transform.position);
+        float speed = velocity.magnitude;
 
+        if (speed > 1f)
+        {
+            float angleOfAttack = GetAngleOfAttack(velocity);
+            float zeroLiftAngle = GetZeroLiftAngle();
+            float liftCoefficient = GetLiftCoefficient(angleOfAttack, zeroLiftAngle);
+
+            Vector3 liftForce = Vector3.zero;
+            Vector3 dragForce = Vector3.zero;
+
+            _rb.AddForceAtPosition(liftForce + dragForce, transform.position);
+        }
+    }
+
+    /// <summary>
+    /// Calculates the angle of attack
+    /// </summary>
+    /// <param name="velocity">The velocity at the surface position</param>
+    /// <returns>The angle of attack</returns>
+    private float GetAngleOfAttack(Vector3 velocity)
+    {
+        Vector3 chordLine = transform.forward;
+        Vector3 projectedVelocity = Vector3.zero;
+
+        float angleOfAttck = 0f;
+        if (LiftAxis == WingAxis.Horizontal)
+        {
+            projectedVelocity = Vector3.ProjectOnPlane(velocity.normalized, transform.right);
+            angleOfAttck = Vector3.SignedAngle(chordLine, projectedVelocity, transform.right);
+        }
+        else if (LiftAxis == WingAxis.Vertical)
+        {
+            projectedVelocity = Vector3.ProjectOnPlane(velocity.normalized, transform.up);
+            angleOfAttck = Vector3.SignedAngle(chordLine, projectedVelocity, transform.up);
+        }
+
+        return angleOfAttck * Mathf.Deg2Rad;
+    }
+
+    /// <summary>
+    /// Calculates the angle of attack where the lift is zero
+    /// </summary>
+    /// <returns></returns>
+    private float GetZeroLiftAngle()
+    {
+        float deflection = (ControlSurfaceDeflection * Mathf.Deg2Rad) * DeflectionCoefficient;
+        float zeroLiftAngle = InitialZeroLiftAngle + deflection;
+
+        return zeroLiftAngle;
+    }
+
+    /// <summary>
+    /// Calculates the lift coefficient
+    /// </summary>
+    /// <param name="angleOfAttack">The angle of attack at this surface</param>
+    /// <param name="zeroLiftAngle">The zero lift angle</param>
+    /// <returns>The lift coefficient</returns>
+    private float GetLiftCoefficient(float angleOfAttack, float zeroLiftAngle)
+    {
+        float liftCoefficient = 2f * Mathf.PI * (angleOfAttack - zeroLiftAngle);
+
+        return liftCoefficient;
     }
 
     /// <summary>
